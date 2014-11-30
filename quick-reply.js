9cb76a32296b57d7278b05a57d657aa1501282c0
DeepanshuDialani <deepdialani42@gmail.com>
2014-11-30 14:18:16 +0530

    Quick Reply post request to server

--- quick-reply.js
+++ quick-reply.js
@@ -1,7 +1,9 @@
+var fromPhone;
+
 function show(details)
 {
 	var msg = details['msg'];
-	var fromPhone = details['sender'];
+	fromPhone = details['sender'];
 	chrome.runtime.onMessage.removeListener(fetchData);
 	document.getElementById('container').style.display = 'block';
 	document.getElementById('image').src = '/ic_launcher.png';
@@ -18,5 +20,52 @@ function fetchData(req, sender, sendResponse)
     	show(details);
   	}
 }
+
+function sendMessage()
+{
+	pin = $("#pin").val();
+	chrome.storage.local.get('msisdn', function(result)
+    {
+        var msisdn = result.msisdn;
+        console.log('msisdn: ', result.msisdn);
+        var time = $.now();
+        var message = $("#message").val();;
+        var fromTo = "{\"f\":" + "\"" + msisdn + "\""+ ", \"to\":" +"\"" + fromPhone + "\"";
+        var md = ", \"md\":{\"sub\":\"desktop\"}";
+        var data = ", \"d\":{\"ts\":" + time + ", \"i\":" + time + ", \"hm\":" + "\"" + message + "\"}, \"t\":\"m\"}"
+        data = fromTo + md + data;
+        console.log("data = " + data);
+         var xhr = $.ajax({
+            type: "POST",
+            url: "http://staging.im.hike.in/v1/pin_validate", 
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(msg) {
+                console.log("Server response = " + $.param(msg));
+                if(msg.stat == "ok")
+                {
+                    console.log("Message sent.");                   
+                }
+                else
+                {
+                    
+                }
+            },
+            error: function(err) {
+                console.log("Error sending request");
+            }
+        });
+        console.log("xhr = " + xhr); 
+    });
+
+       
+}
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        sendMessage();
+    }
+});
 //Adding message listener for updating UI
 chrome.runtime.onMessage.addListener(fetchData);
