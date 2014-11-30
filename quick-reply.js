ea20cbbda99e8ab7eab7a4be207e8a7308796e6a
piyushghai <piyush@hike.in>
2014-11-30 11:38:52 +0530

    Added map to maintain uniqueness across notifications

--- quick-reply.js
+++ quick-reply.js
@@ -2,19 +2,21 @@ function show(details)
 {
 	var msg = details['msg'];
 	var fromPhone = details['sender'];
-console.log("Inside JS file" + details);
-document.getElementById('container').style.display = 'block';
-document.getElementById('image').src = '/ic_launcher.png';
-document.getElementById('title').textContent = fromPhone;
-document.getElementById('desc').textContent = 'Via Hike Messenger';
-document.getElementById('message').textContent = msg;
+	chrome.runtime.onMessage.removeListener(fetchData);
+	document.getElementById('container').style.display = 'block';
+	document.getElementById('image').src = '/ic_launcher.png';
+	document.getElementById('title').textContent = fromPhone;
+	document.getElementById('desc').textContent = 'Via Hike Messenger';
+	document.getElementById('message').textContent = msg;
 }
 
-chrome.runtime.onMessage.addListener(
-  function(req, sender, sendResponse) {
-  	console.log("Adding listener");
-  if (req.details) {
-    var details = req.details.message;
-    show(details);
-  }
-});
+function fetchData(req, sender, sendResponse)
+{
+	console.log("Adding listener");
+  	if (req.details) {
+    	var details = req.details.message;
+    	show(details);
+  	}
+}
+//Adding message listener for updating UI
+chrome.runtime.onMessage.addListener(fetchData);
